251e740d6caadaa4990f3dd303f40d2aeba03777
PIRODEVELEPOR <82955298+hk4crprasad@users.noreply.github.com>
2024-01-04 20:17:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 from gemini_pro_bot.bot import start_bot
 
+from flask import Flask,render_template
+from threading import Thread
+import os
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return "Alive"
+def run():
+  app.run(host='0.0.0.0',port=8080)
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
 if __name__ == "__main__":
+    keep_alive()
     start_bot()
